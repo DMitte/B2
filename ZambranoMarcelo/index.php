68dd10a33d3c0a0c179946adfca369199357632d
DMitte <mittedany547@gmail.com>
2023-05-02 23:31:43 -0500

    nuevos archivos

---
+++ ZambranoMarcelo/index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title>
+  Pagina de Zambrano Marcelo
+
+</title>
+</head>
+<body>
+    <div   style="border:2px red solid;  background-color:gray;font-size:40px">
+  HOJA DE VIDA
+</div>
+<div>
+   CUERPO DE LA HOJA DE VIDA
+</div>
+<div>
+   FOOTHER DE LA HOJA DE VIDA
+</div>
+</body>
+
+
+
+
+</html>
